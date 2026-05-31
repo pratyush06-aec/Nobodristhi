bc4c94e900fd99c67730e6a696acc85c18819823
kingmon6996 <deadpoolroy2006@gmail.com>
2026-05-31 09:44:25 +0530

    Update CORS configuration to allow all origins for broader access

--- main.py
+++ main.py
@@ -20,14 +20,7 @@ log.setLevel(logging.ERROR)
 
 # Configure CORS
 CORS(app, 
-     origins=[
-         "http://localhost:3000",
-         "http://localhost:5173",
-         "http://127.0.0.1:8080",
-         "http://127.0.0.1:5173",
-         "https://nobodorshi.kingmon.xyz",
-         "http://nobodorshi.kingmon.xyz"
-     ],
+     origins=["*"],
      allow_headers=["Content-Type", "Authorization"],
      methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
      supports_credentials=True,
